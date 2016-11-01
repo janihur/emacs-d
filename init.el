85c7fc8165efdf0f5f58c898e814b425d5db0ad8
Jani Hur <webmaster@jani-hur.net>
2016-11-01 21:12:14 +0200

    MELPA setup.

--- init.el
+++ init.el
@@ -36,12 +36,54 @@
 ;;
 
 ;;
-;;
+;; MELPA (https://melpa.org/#/getting-started)
 ;;
 
-(package-initialize)
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+;; Use stable (https://stable.melpa.org/#/)
+
+;; Note: There are some problems using the https location with Emacs
+;; on Windows. There is currently no know easy fix for this. You can
+;; still use MELPA by using the non-SSL location by replacing https
+;; with http.
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(package-initialize) ;; You might already have this line
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (markdown-mode plsql))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;
+;; Package setup: markdown-mode
+;;
+
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+;(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;;
+;; Package setup: plsql-mode
+;;
 
 (require 'plsql)
 (setq plsql-indent 2)
